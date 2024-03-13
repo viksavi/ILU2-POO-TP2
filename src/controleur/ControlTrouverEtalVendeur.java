8d24a6e2270cdf7cd2884fee16f163f7c4d50709
viksavi <viktoriia.yarmolich@univ-tlse3.fr>
2024-03-13 11:43:03 +0100

    ControleurTrouverEtalVendeur

--- src/controleur/ControlTrouverEtalVendeur.java
+++ src/controleur/ControlTrouverEtalVendeur.java
@@ -2,6 +2,7 @@ package controleur;
 
 import villagegaulois.Etal;
 import villagegaulois.Village;
+import personnages.Gaulois;
 
 public class ControlTrouverEtalVendeur {
 	private Village village;
@@ -11,8 +12,11 @@ public class ControlTrouverEtalVendeur {
 	}
 
 	public Etal trouverEtalVendeur(String nomVendeur) {
-		//TODO a completer
 		Etal etal = null;
+		Gaulois gaulois = village.trouverHabitant(nomVendeur);
+		if(gaulois != null) {
+			etal = village.rechercherEtal(gaulois);
+		}
 		return etal;
 	}
 }
