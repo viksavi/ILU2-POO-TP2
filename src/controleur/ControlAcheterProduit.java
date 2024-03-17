f51309ea4b93e7b6df59d73668146a5e0f464017
viksavi <viktoriia.yarmolich@univ-tlse3.fr>
2024-03-17 20:24:48 +0100

    Acheter produit

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -1,5 +1,7 @@
 package controleur;
 
+import personnages.Gaulois;
+import villagegaulois.Etal;
 import villagegaulois.Village;
 
 public class ControlAcheterProduit {
@@ -14,6 +16,42 @@ public class ControlAcheterProduit {
 		this.controlVerifierIdentite = controlVerifierIdentite;
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
-
-	//TODO a completer
+	
+	public boolean verifierIdentite(String nomAcheteur) {
+		boolean nomAcheteurConnu = false;
+		nomAcheteurConnu = controlVerifierIdentite.verifierIdentite(nomAcheteur);
+		return nomAcheteurConnu;
+	}
+	
+	public boolean isVendeur(String nomVendeur) {
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		return (etal != null);
+	}
+	
+	public Gaulois[] produitExiste(String produit) {
+		return village.rechercherVendeursProduit(produit);
+	}
+	
+	public int acheterProduit(Gaulois gaulois, int nbAcheter) {
+		Etal etal = village.rechercherEtal(gaulois);
+		return etal.acheterProduit(nbAcheter);
+	}
+	
+	public String afficherVendeurs(Gaulois[] vendeurs) {
+		StringBuilder result = new StringBuilder();
+		for(int i = 0; i < vendeurs.length; i++) {
+			result.append(i+1);
+			result.append(" - ");
+			result.append(vendeurs[i].getNom());
+			result.append("\n");
+		}
+		return result.toString();
+	}
+	
+	public Gaulois trouverGaulois(Gaulois[] vendeurs, int numEtal) {
+		if(numEtal-1 >= 0 && numEtal - 1 < vendeurs.length) {
+			return vendeurs[numEtal-1];
+		}
+		return null;
+	}
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -3,6 +3,7 @@ package frontiere;
 import java.util.Scanner;
 
 import controleur.ControlAcheterProduit;
+import personnages.Gaulois;
 
 public class BoundaryAcheterProduit {
 	private Scanner scan = new Scanner(System.in);
@@ -13,6 +14,85 @@ public class BoundaryAcheterProduit {
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		// TODO à completer
+		boolean nomAcheteurConnu = false;
+		nomAcheteurConnu = controlAcheterProduit.verifierIdentite(nomAcheteur);
+		
+		if(!nomAcheteurConnu) {
+			StringBuilder message = new StringBuilder();
+			message.append("Je suis désolée ");
+			message.append(nomAcheteur);
+			message.append(" mais il faut être un habitant de notre village pour acheter ici.");
+			System.out.println(message.toString());
+		} else {
+			System.out.println("Quel produit voulez-vous acheter ?");
+			String produit = scan.nextLine();
+			Gaulois[] vendeurs = controlAcheterProduit.produitExiste(produit);
+			if(vendeurs == null) {
+				System.out.println("Désolé, personne ne vend ce produit au marché.");
+			} else {
+				StringBuilder message2 = new StringBuilder();
+				message2.append("Chez quel commercant voulez-vous acheter des ");
+				message2.append(produit);
+				message2.append(" ?\n");
+				message2.append(controlAcheterProduit.afficherVendeurs(vendeurs));
+				int numEtal = Clavier.entrerEntier(message2.toString());
+				/*try {
+					Gaulois vendeur = controlAcheterProduit.trouverGaulois(vendeurs, numEtal);
+				} catch(IndexOutOfBoundsException e) {
+					e.printStackTrace();
+					System.out.println("Ce commercant n'existe pas!\n");
+				}*/
+				Gaulois vendeur = controlAcheterProduit.trouverGaulois(vendeurs, numEtal);
+				if(vendeur == null) {
+					System.out.println("Ce commercant n'existe pas!\n");
+				} else {
+					StringBuilder message3 = new StringBuilder();
+					message3.append(nomAcheteur);
+					message3.append(" se déplace jusqu'à l'étal du vendeur Bonemine\n");
+					message3.append("Bonjour ");
+					message3.append(nomAcheteur);
+					message3.append("\n");
+					message3.append("Combien de ");
+					message3.append(produit);
+					message3.append(" voulez-vous acheter ?");
+					int nbAchat = Clavier.entrerEntier(message3.toString());
+					int nbExist = controlAcheterProduit.acheterProduit(vendeur, nbAchat);
+					StringBuilder result = new StringBuilder();
+					if(nbExist == 0) {
+						result.append(nomAcheteur);
+						result.append(" veut acheter ");
+						result.append(nbAchat);
+						result.append(" ");
+						result.append(produit);
+						result.append(", malheureusement il n'y en a plus !");
+					} else if(nbExist == nbAchat) {
+						result.append(nomAcheteur);
+						result.append(" achete ");
+						result.append(nbAchat);
+						result.append(" ");
+						result.append(produit);
+						result.append(" à ");
+						result.append(vendeur.getNom());
+					} else if(nbExist < nbAchat) {
+						result.append(nomAcheteur);
+						result.append(" veut acheter ");
+						result.append(nbAchat);
+						result.append(" ");
+						result.append(produit);
+						result.append(", malheureusemnt ");
+						result.append(vendeur.getNom());
+						result.append(" n'en a plus que ");
+						result.append(nbExist);
+						result.append(". ");
+						result.append(nomAcheteur);
+						result.append(" achète tout le stock de ");
+						result.append(vendeur.getNom());
+						result.append(".");
+					}
+					System.out.println(result.toString());
+				}
+				
+			}
+		}
 	}
 }
