53cebcf99074f2730a010c459034ee430574141a
viksavi <viktoriia.yarmolich@univ-tlse3.fr>
2024-03-29 15:00:01 +0100

    test ControlPrendreEtal

---
+++ test/controleur/ControlPrendreEtalTest.java
@@ -0,0 +1,55 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import personnages.Gaulois;
+import villagegaulois.Village;
+
+class ControlPrendreEtalTest {
+
+	private Village village;
+	private Chef abraracourcix;
+	private Gaulois bonemine;
+	private ControlVerifierIdentite controlVerifierIdentite;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irréductibles", 10, 5);
+		controlVerifierIdentite = new ControlVerifierIdentite(village);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+		bonemine = new Gaulois("Bonemine", 10);
+		village.ajouterHabitant(bonemine);
+	}
+	
+	@Test
+	void testControlPrendreEtal() {
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite, village);
+		assertNotNull(controlPrendreEtal, "Constructeur ne renvoie pas null");
+	}
+
+	@Test
+	void testResteEtals() {
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite, village);
+		assertTrue(controlPrendreEtal.resteEtals());
+	}
+
+	@Test
+	void testPrendreEtal() {
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite, village);
+		assertEquals(controlPrendreEtal.prendreEtal("Bonemine", "fleurs", 10), 1);
+	}
+
+	@Test
+	void testVerifierIdentite() {
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite, village);
+		assertTrue(controlPrendreEtal.verifierIdentite("Bonemine"));
+		assertFalse(controlPrendreEtal.verifierIdentite("Panoramix"));
+	}
+
+}
