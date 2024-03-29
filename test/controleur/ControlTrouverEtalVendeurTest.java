516d6407aaf47b5c3605b0bdef3e6e06e8eb1dc7
viksavi <viktoriia.yarmolich@univ-tlse3.fr>
2024-03-29 15:08:39 +0100

    test ControlTrouverEtalVendeur

---
+++ test/controleur/ControlTrouverEtalVendeurTest.java
@@ -0,0 +1,42 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import personnages.Gaulois;
+import villagegaulois.Village;
+
+class ControlTrouverEtalVendeurTest {
+
+	private Village village;
+	private Chef abraracourcix;
+	private Gaulois bonemine;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+		bonemine = new Gaulois("Bonemine", 10);
+		village.ajouterHabitant(bonemine);
+	}
+	
+	@Test
+	void testControlTrouverEtalVendeur() {
+		ControlTrouverEtalVendeur controlTrouverEtalVendeur = new ControlTrouverEtalVendeur(village);
+		assertNotNull(controlTrouverEtalVendeur, "Constructeur ne renvoie pas null");
+	}
+
+	@Test
+	void testTrouverEtalVendeur() {
+		ControlTrouverEtalVendeur controlTrouverEtalVendeur = new ControlTrouverEtalVendeur(village);
+		assertNull(controlTrouverEtalVendeur.trouverEtalVendeur(bonemine.getNom()));
+		village.installerVendeur(bonemine, "fleurs", 10);
+		assertNotNull(controlTrouverEtalVendeur.trouverEtalVendeur(bonemine.getNom()));
+	}
+
+}
