0cb65cfb44cb0e9d3d9c904f39c73cc2fae66d1d
viksavi <viktoriia.yarmolich@univ-tlse3.fr>
2024-03-29 14:33:16 +0100

    test ControlAfficherVillageTest

---
+++ test/controleur/ControlAfficherVillageTest.java
@@ -0,0 +1,49 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlAfficherVillageTest {
+
+	private Village village;
+	private Chef abraracourcix;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+	}
+	
+	@Test
+	void testControlAfficherVillage() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		assertNotNull(controlAfficherVillage, "Constructeur ne renvoie pas null");
+	}
+
+	@Test
+	void testDonnerNomsVillageois() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		assertEquals(controlAfficherVillage.donnerNomsVillageois()[0], "Abraracourcix");
+		assertEquals(controlAfficherVillage.donnerNomsVillageois().length, 1);
+	}
+
+	@Test
+	void testDonnerNomVillage() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		assertEquals(controlAfficherVillage.donnerNomVillage(), "le village des irréductibles");
+	}
+
+	@Test
+	void testDonnerNbEtals() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		assertEquals(controlAfficherVillage.donnerNbEtals(), 5);
+	}
+
+}
