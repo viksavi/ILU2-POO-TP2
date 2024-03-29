28a827377ed813541d3b44c1e830849a318a2a27
viksavi <viktoriia.yarmolich@univ-tlse3.fr>
2024-03-29 14:44:54 +0100

    test ControlVerifierIdentiteTest

---
+++ test/controleur/ControlVerifierIdentiteTest.java
@@ -0,0 +1,41 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import personnages.Gaulois;
+import villagegaulois.Village;
+
+class ControlVerifierIdentiteTest {
+
+	private Village village;
+	private Chef abraracourcix;
+	private Gaulois bonemine;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+		bonemine = new Gaulois("Bonemine", 10);
+		village.ajouterHabitant(bonemine);
+	}
+	
+	@Test
+	void testControlVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		assertNotNull(controlVerifierIdentite, "Constructeur ne renvoie pas null");
+	}
+
+	@Test
+	void testVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		assertTrue(controlVerifierIdentite.verifierIdentite("Bonemine"));
+		assertFalse(controlVerifierIdentite.verifierIdentite("Panoramix"));
+	}
+
+}
